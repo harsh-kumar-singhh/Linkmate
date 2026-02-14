40b0db599bf84489c123af0337c86e31b8a4a04a
Harsh Kumar Singh <rakeshsingh133754@gmail.com>
2026-02-14 16:19:09 +0530

    final lauch fix done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({
           <ThemeSync />
           <ClientLayout>{children}</ClientLayout>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
